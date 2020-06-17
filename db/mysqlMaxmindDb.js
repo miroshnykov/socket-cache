41e217d719f509232e5077737770f0027c9cbe50
miroshnykov <dmytro.miroshnykov@adsurge.com>
2020-06-17 16:09:34 -0400

    maxmind config

--- db/mysqlMaxmindDb.js
+++ db/mysqlMaxmindDb.js
@@ -7,7 +7,8 @@ module.exports = {
         if (!mysqlAdcenterDb) {
             console.log(`\n \x1b[35m First init adcenter DB \x1b[0m`)
             console.log(config.db.maxmind)
-            const {host, database, user, password, port} = config.db.maxmind
+            const {host, user, password, port} = config.db.adcenter
+            const {database} = config.db.maxmind
             let mysqlConfig = {
                 host: host,
                 database: database,
